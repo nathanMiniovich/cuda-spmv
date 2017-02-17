e64ce16fb5173aa5028be3686cb1c825f3516224
Augustus Chang <augustus.chang@rutgers.edu>
2017-02-17 01:08:42 -0500

    add kernel func for mult

--- src/spmv_segment.c
+++ src/spmv_segment.c
@@ -18,10 +18,31 @@ __device__ void segmented_scan(const int lane, const int *rows, float *vals){
 	if ( lane >= 16 && rows[threadIdx.x] == rows[threadIdx.x - 16] )
 		vals[threadIdx.x] += vals[threadIdx.x - 16];
 }
+
 __global__ void putProduct_kernel(/*Arguments*/){
     /*Put your kernel(s) implementation here, you don't have to use exactly the
  * same kernel name */
 }
+
+
+__global__ void getMulAtomic_kernel(const int nnz, const int* coord_row, const int* coord_col, const float* A, const float* x, float* y){
+        int thread_id = blockDim.x * blockIdx.x + threadIdx.x;
+        int thread_num = blockDim.x * gridDim.x;
+        int iter = nnz % thread_num ? nnz/thread_num + 1: nnz/thread_num;
+
+        for(int i = 0; i < iter; i++){
+                int dataid = thread_id + i * thread_num;
+                if(dataid < nnz){
+                        float data = A[dataid];
+                        int row = coord_row[dataid];
+                        int col = coord_col[dataid];
+                        float tmp = data * x[col];
+                        atomicAdd(&y[row], tmp);
+                }
+        }
+}
+
+
 
 void getMulScan(MatrixInfo * mat, MatrixInfo * vec, MatrixInfo * res, int blockSize, int blockNum){
     /*Allocate things...*/
