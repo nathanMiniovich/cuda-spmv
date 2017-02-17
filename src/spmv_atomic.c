91b98902656319b502bfadf94ab391700c232252
nathanMiniovich <nathan.miniovich@rutgers.edu>
2017-02-17 00:09:43 -0500

    cleanup code

--- src/spmv_atomic.c
+++ src/spmv_atomic.c
@@ -19,33 +19,34 @@ __global__ void getMulAtomic_kernel(const int nnz, const int* coord_row, const i
 }
 
 void getMulAtomic(MatrixInfo * mat, MatrixInfo * vec, MatrixInfo * res, int blockSize, int blockNum){
-	/*Allocate here...*/
 	int nnz = mat->nz;
 	int M = mat->M;
 	int *coord_row, *coord_col;
 	float *A, *x, *y;
+
 	cudaMallocManaged((void**)&coord_row, (size_t)nnz*sizeof(int));
 	cudaMallocManaged((void**)&coord_col, (size_t)nnz*sizeof(int));
 	cudaMallocManaged((void**)&A, (size_t)nnz*sizeof(float));
 	cudaMallocManaged((void**)&x, (size_t)M*sizeof(float));
 	cudaMallocManaged((void**)&y, (size_t)M*sizeof(float));
-	/* Initialize here...*/
+	
 	cudaMemcpy(coord_row, mat->rIndex, (size_t)nnz*sizeof(int), cudaMemcpyHostToDevice);
 	cudaMemcpy(coord_col, mat->cIndex, (size_t)nnz*sizeof(int), cudaMemcpyHostToDevice);
 	cudaMemcpy(A, mat->val, (size_t)nnz*sizeof(float), cudaMemcpyHostToDevice);
 	cudaMemcpy(x, vec->val, (size_t)M*sizeof(float), cudaMemcpyHostToDevice);
 	cudaMemset(y, 0, (size_t)M*sizeof(float));
-	/* Sample timing code */
+	
 	struct timespec start, end;
 	clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-	/*Invoke kernels...*/
+	
 	getMulAtomic_kernel<<<blockNum, blockSize>>>(nnz, coord_row, coord_col, A, x, y);
+	
 	cudaDeviceSynchronize();
 	clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-	printf("Atomic Kernelll Time: %lu micro-seconds\n", 1000000 * (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000);
-	/* Write back result */
+	printf("Atomic Kernel Time: %lu micro-seconds\n", 1000000 * (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000);
+	
 	cudaMemcpy(res->val, y, (size_t)M*sizeof(float), cudaMemcpyDeviceToHost);
-	/*Deallocate.*/
+	
 	cudaFree(coord_row);
 	cudaFree(coord_col);
 	cudaFree(A);
