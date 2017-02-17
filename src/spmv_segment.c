999f0af29058ce544b48f3de006da1ecd82bad55
Augustus Chang <augustus.chang@rutgers.edu>
2017-02-17 00:28:49 -0500

    add device func

--- src/spmv_segment.c
+++ src/spmv_segment.c
@@ -1,7 +1,23 @@
 #include "genresult.cuh"
 #include <sys/time.h>
 
-
+__device__ void segmented_scan(const int lane, const int *rows, float *vals){
+	// segmented scan in shared memory, assuming corresponding A values
+	// are loaded into the shared memory array vals, the row indices loaded
+	// into rows[] array in shared memory
+	// lane is the thread offset in the thread warp
+	
+	if ( lane >= 1 && rows[threadIdx.x] == rows[threadIdx.x - 1] )
+		vals[threadIdx.x] += vals[threadIdx.x - 1];
+	if ( lane >= 2 && rows[threadIdx.x] == rows[threadIdx.x - 2] )
+		vals[threadIdx.x] += vals[threadIdx.x - 2];
+	if ( lane >=4 && rows[threadIdx.x] == rows[threadIdx.x - 4] )
+		vals[threadIdx.x] += vals[threadIdx.x - 4];
+	if ( lane >= 8 && rows[threadIdx.x] == rows[threadIdx.x - 8] )
+		vals[threadIdx.x] += vals[threadIdx.x - 8];
+	if ( lane >= 16 && rows[threadIdx.x] == rows[threadIdx.x - 16] )
+		vals[threadIdx.x] += vals[threadIdx.x - 16];
+}
 __global__ void putProduct_kernel(/*Arguments*/){
     /*Put your kernel(s) implementation here, you don't have to use exactly the
  * same kernel name */
