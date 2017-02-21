28261a17d1390de089e4ea265cd14a2fbccb05a9
Augustus Chang <augustus.chang@rutgers.edu>
2017-02-21 17:36:51 -0500

    remove global alloc.

--- src/spmv_segment.c
+++ src/spmv_segment.c
@@ -68,7 +68,7 @@ void getMulScan(MatrixInfo * mat, MatrixInfo * vec, MatrixInfo * res, int blockS
     	struct timespec start, end;
     	clock_gettime(CLOCK_MONOTONIC_RAW, &start);
     	/*Invoke kernel(s)*/
-	putProduct_kernel<<<blockNum, blockSize, 2*nnz>>>(N, nnz, coord_row, coord_col, A, x, y);
+	putProduct_kernel<<<blockNum, blockSize>>>(N, nnz, coord_row, coord_col, A, x, y);
     	cudaDeviceSynchronize();
     	clock_gettime(CLOCK_MONOTONIC_RAW, &end);
     	printf("Segmented Kernel Time: %lu micro-seconds\n", 1000000 * (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000);
