2bdf3e477013fb1a9860e4eeaab1de405a955193
nathanMiniovich <nathan.miniovich@rutgers.edu>
2017-02-22 04:24:19 -0500

    added skeleton for design kernel

--- src/spmv_design.c
+++ src/spmv_design.c
@@ -1,19 +1,43 @@
 #include "genresult.cuh"
 #include <sys/time.h>
 
-/* Put your own kernel(s) here*/
+__global__ void designer_kernel(const int nnz, const int* coord_row, const int* coord_col, const float* A, const float* x, float* y){
+
+}
 
 void getMulDesign(MatrixInfo * mat, MatrixInfo * vec, MatrixInfo * res, int blockSize, int blockNum){
-    /*Allocate*/
+	int nnz = mat->nz;
+	int numRows = mat->M;
+	int numCols = mat->N;
+	int *coord_row, *coord_col;
+	float *A, *x, *y;
 
-    struct timespec start, end;
-    cudaDeviceSynchronize();
-    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-    /*Your own magic here!*/
+	cudaMalloc((void**)&coord_row, (size_t)nnz*sizeof(int));
+	cudaMalloc((void**)&coord_col, (size_t)nnz*sizeof(int));
+	cudaMalloc((void**)&A, (size_t)nnz*sizeof(float));
+	cudaMalloc((void**)&x, (size_t)numCols*sizeof(float));
+	cudaMalloc((void**)&y, (size_t)numRows*sizeof(float));
 
-    cudaDeviceSynchronize();
-    clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-    printf("Your Own Kernel Time: %lu micro-seconds\n", 1000000 * (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000);
+	cudaMemcpy(coord_row, mat->rIndex, (size_t)nnz*sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(coord_col, mat->cIndex, (size_t)nnz*sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(A, mat->val, (size_t)nnz*sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(x, vec->val, (size_t)numCols*sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemset(y, 0, (size_t)numRows*sizeof(float));
 
-    /*Deallocate*/
+	struct timespec start, end;
+	clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+
+	designer_kernel<<<blockNum, blockSize>>>(nnz, coord_row, coord_col, A, x, y);
+
+	cudaDeviceSynchronize();
+	clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+	printf("Your Own Kernel Time: %lu micro-seconds\n", 1000000 * (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000);
+
+	cudaMemcpy(res->val, y, (size_t)numRows*sizeof(float), cudaMemcpyDeviceToHost);
+
+	cudaFree(coord_row);
+	cudaFree(coord_col);
+	cudaFree(A);
+	cudaFree(x);
+	cudaFree(y);
 }
